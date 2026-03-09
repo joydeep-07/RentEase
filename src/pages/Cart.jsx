1323698dc8eeebe5d9473ec59d89661aa2decb00
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-03-09 10:46:32 +0530

    Updatd ui

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -124,12 +124,17 @@ const Cart = () => {
         <hr className="border-[var(--border-light)] my-5" />
 
         <div className="mb-6">
-          <p className="text-sm font-medium uppercase text-[var(--text-secondary)]">
+          <p className="text-sm font-medium uppercase text-[var(--accent-blue)]">
             Delivery Address
           </p>
 
           <div className="relative flex justify-between items-start mt-2">
-            <p className="text-[var(--text-secondary)]">No address found</p>
+            <p className="text-[var(--text-secondary)]">
+              <p>
+               Vill - Alladi II, Post - Basudevpur Jemari, Asansol, Paschim Bardhaman,
+                West Bengal, India, 713335.
+              </p>
+            </p>
 
             <button
               onClick={() => setShowAddress(!showAddress)}
@@ -137,34 +142,7 @@ const Cart = () => {
             >
               Change
             </button>
-
-            {showAddress && (
-              <div className="absolute top-12 py-1 bg-[var(--bg-main)] border border-[var(--border-light)] text-sm w-full rounded">
-                <p
-                  onClick={() => setShowAddress(false)}
-                  className="text-[var(--text-secondary)] p-2 hover:bg-[var(--bg-secondary)]"
-                >
-                  New York, USA
-                </p>
-
-                <p
-                  onClick={() => setShowAddress(false)}
-                  className="text-[var(--accent-primary)] text-center cursor-pointer p-2 hover:bg-[var(--accent-primary)]/10"
-                >
-                  Add address
-                </p>
-              </div>
-            )}
           </div>
-
-          <p className="text-sm font-medium uppercase mt-6 text-[var(--text-secondary)]">
-            Payment Method
-          </p>
-
-          <select className="w-full border border-[var(--border-light)] bg-[var(--bg-main)] px-3 py-2 mt-2 outline-none text-[var(--text-main)] rounded">
-            <option value="COD">Cash On Delivery</option>
-            <option value="Online">Online Payment</option>
-          </select>
         </div>
 
         <hr className="border-[var(--border-light)]" />
@@ -192,7 +170,7 @@ const Cart = () => {
         </div>
 
         <button className="w-full py-3 mt-6 cursor-pointer bg-[var(--accent-primary)] text-white font-medium hover:opacity-90 transition rounded">
-          Place Order
+         Proceed to payment
         </button>
       </div>
     </div>
