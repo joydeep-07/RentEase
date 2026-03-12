56a5f23dcf5b3da22c81ab3036b1aa0c6c00967f
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-03-12 08:04:56 +0530

    Curved Loop Component Created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Hero from '../components/Hero';
 import Testimonials from '../components/Testimonials';
+import CurvedLoop from '../components/CurvedLoop';
 
 const Home = () => {
     
   return (
     <>
-    <Hero/>
-    <Testimonials/>
+      <Hero />
+      <CurvedLoop
+        marqueeText="Rent what you need when you need it without buying."
+        speed={2}
+        curveAmount={-310}
+        direction="right"
+        interactive
+        className="text-5xl"
+      />
+      <Testimonials />
     </>
   );
 }
